252f4d915b5b06125af4f5ceb563ae0c0f4ff847
Hoju Choi <5stralia@gmail.com>
2024-01-11 20:48:38 +0900

    feat: Pretendard 폰트 추가

---
+++ MoneyBook/Extension/Font+custom.swift
@@ -0,0 +1,14 @@
+//
+//  Font+custom.swift
+//  MoneyBook
+//
+//  Created by Hoju Choi on 1/9/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static func Pretendard(size: CGFloat) -> Font {
+        Font.custom("Pretendard", size: size)
+    }
+}
