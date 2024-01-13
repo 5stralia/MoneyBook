91ae3a072629b16db6a2bd3d6acae20cf33605e8
Hoju Choi <5stralia@gmail.com>
2024-01-13 15:50:00 +0900

    refactor: 불용코드 제거

--- MoneyBook/Chart/ChartView.swift
+++ MoneyBook/Chart/ChartView.swift
@@ -9,19 +9,6 @@ import Charts
 import Foundation
 import SwiftUI
 
-private struct ValuePerCategory {
-    var category: String
-    var value: Double
-}
-
-private struct ChartData: Identifiable {
-    let category: String
-    let value: Double
-    let yearMonth: String
-
-    let id: UUID = UUID()
-}
-
 enum Recording {
     case expense
     case income
@@ -37,13 +24,6 @@ struct ChartView: View {
 
     @State private var year = 2023
     @State private var month = 1
-    @State private var chartVisibleLength = 4
-
-    @State private var isItemsLoaded: Bool = false
-    @State private var selection1: [String] = []
-    @State private var selection2: [String] = []
-
-    @State private var pickerSelection: Recording = .expense
 
     @State var monthlyCategoryItems: [MonthlyCategoryItem] = [
         .init(ratio: 0.66, title: "식비", value: 759090),
@@ -84,179 +64,20 @@ struct ChartView: View {
             let date = Date()
             self.year = date.getYear()
             self.month = date.getMonth()
-
-            if !self.isItemsLoaded {
-                if self.selection1.isEmpty { self.selection1 = self.getAllCategories(isIncome: false) }
-                if self.selection2.isEmpty { self.selection2 = self.getAllCategories(isIncome: true) }
-
-                self.isItemsLoaded = true
-            }
         })
     }
 
-    private func getAllCategories(isIncome: Bool) -> [String] {
-        let filtered = self.items.filter { item in
-            if isIncome {
-                return item.amount >= 0
-            } else {
-                return item.amount < 0
-            }
-        }
-
-        return Dictionary(grouping: filtered, by: { $0.category }).map { $0.key }
-    }
-
-    private func filterCurrentMonthItems(
-        _ items: [ItemCoreEntity],
-        year: Int,
-        month: Int,
-        isIncome: Bool
-    ) -> [ChartData] {
-        let items = items.filter { $0.timestamp.getYear() == year && $0.timestamp.getMonth() == month }
-            .filter { isIncome ? $0.amount < 0 : $0.amount >= 0 }
-
-        let grouping = Dictionary(grouping: items, by: { $0.category })
-        return grouping.map { (key, value) in
-            ChartData(
-                category: key,
-                value: value.filter({ isIncome ? $0.amount < 0 : $0.amount > 0 })
-                    .map({ isIncome ? -$0.amount : $0.amount })
-                    .reduce(0, +),
-                yearMonth: "\(year). \(month)"
-            )
-        }
-        .sorted(using: KeyPathComparator(\.value, order: .reverse))
-    }
-
-    private func filteredItems(
-        _ items: [ItemCoreEntity],
-        year: Int,
-        month: Int,
-        length: Int,
-        isIncome: Bool,
-        selctedCategories: [String]
-    ) -> [ChartData] {
-        var result: [ChartData] = []
-
-        let items = items.filter { item in
-            let minDiff = month - length
-            let minMonth = (minDiff <= 0) ? (12 + minDiff) : minDiff
-            let minYear = (minDiff <= 0) ? (year - 1) : year
-
-            let maxDiff = month + length
-            let maxMonth = (maxDiff > 12) ? (maxDiff - 12) : maxDiff
-            let maxYear = (maxDiff > 12) ? (year + 1) : year
-
-            guard
-                let minDate = Calendar.current.date(from: DateComponents(year: minYear, month: minMonth)),
-                let maxDate = Calendar.current.date(from: DateComponents(year: maxYear, month: maxMonth))
-            else {
-                return false
-            }
-
-            if isIncome {
-                return item.amount >= 0 && minDate <= item.timestamp && item.timestamp <= maxDate
-            } else {
-                return item.amount < 0 && minDate <= item.timestamp && item.timestamp <= maxDate
-            }
-        }
-        .filter { selctedCategories.contains($0.category) }
-
-        let grouping = Dictionary(grouping: items, by: { $0.category })
-        for (category, values) in grouping {
-            let gp = Dictionary(
-                grouping: values, by: { "\($0.timestamp.getYear()). \($0.timestamp.getMonth())" })
-            for (yearMonth, values2) in gp {
-                if isIncome {
-                    result.append(
-                        ChartData(
-                            category: category, value: values2.map(\.amount).reduce(0, +), yearMonth: yearMonth))
-                } else {
-                    result.append(
-                        ChartData(
-                            category: category, value: values2.map(\.amount).reduce(0, -), yearMonth: yearMonth))
-                }
-            }
-        }
-
-        return result.sorted(using: KeyPathComparator(\.yearMonth))
-            .sorted(using: KeyPathComparator(\.value, order: .reverse))
-    }
-
-    private func incomeData(_ items: [ItemCoreEntity], year: Int, month: Int, length: Int)
-        -> [ChartData]
-    {
-        var result: [ChartData] = []
-
-        let items = items.filter { item in
-            let minDiff = month - length
-            let minMonth = (minDiff <= 0) ? (12 + minDiff) : minDiff
-            let minYear = (minDiff <= 0) ? (year - 1) : year
-
-            let maxDiff = month + length
-            let maxMonth = (maxDiff > 12) ? (maxDiff - 12) : maxDiff
-            let maxYear = (maxDiff > 12) ? (year + 1) : year
-
-            guard
-                let minDate = Calendar.current.date(from: DateComponents(year: minYear, month: minMonth)),
-                let maxDate = Calendar.current.date(from: DateComponents(year: maxYear, month: maxMonth))
-            else {
-                return false
-            }
-
-            return item.amount >= 0 && minDate <= item.timestamp && item.timestamp <= maxDate
-        }
-
-        let grouping = Dictionary(grouping: items, by: { $0.category })
-        for (category, values) in grouping {
-            let gp = Dictionary(
-                grouping: values, by: { "\($0.timestamp.getYear()). \($0.timestamp.getMonth())" })
-            for (yearMonth, values2) in gp {
-                result.append(
-                    ChartData(
-                        category: category, value: values2.map(\.amount).reduce(0, +), yearMonth: yearMonth))
-            }
-        }
-
-        return result.sorted(using: KeyPathComparator(\.yearMonth))
-    }
-
-    private func expendData(_ items: [ItemCoreEntity], year: Int, month: Int, length: Int)
-        -> [ChartData]
-    {
-        var result: [ChartData] = []
-
-        let items = items.filter { item in
-            let minDiff = month - length
-            let minMonth = (minDiff <= 0) ? (12 + minDiff) : minDiff
-            let minYear = (minDiff <= 0) ? (year - 1) : year
-
-            let maxDiff = month + length
-            let maxMonth = (maxDiff > 12) ? (maxDiff - 12) : maxDiff
-            let maxYear = (maxDiff > 12) ? (year + 1) : year
-
-            guard
-                let minDate = Calendar.current.date(from: DateComponents(year: minYear, month: minMonth)),
-                let maxDate = Calendar.current.date(from: DateComponents(year: maxYear, month: maxMonth))
-            else {
-                return false
-            }
-
-            return item.amount < 0 && minDate <= item.timestamp && item.timestamp <= maxDate
-        }
-        let grouping = Dictionary(grouping: items, by: { $0.category })
-        for (category, values) in grouping {
-            let gp = Dictionary(
-                grouping: values, by: { "\($0.timestamp.getYear()). \($0.timestamp.getMonth())" })
-            for (yearMonth, values2) in gp {
-                result.append(
-                    ChartData(
-                        category: category, value: values2.map(\.amount).reduce(0, -), yearMonth: yearMonth))
-            }
-        }
-
-        return result.sorted(using: KeyPathComparator(\.yearMonth))
-    }
+    //    private func getAllCategories(isIncome: Bool) -> [String] {
+    //        let filtered = self.items.filter { item in
+    //            if isIncome {
+    //                return item.amount >= 0
+    //            } else {
+    //                return item.amount < 0
+    //            }
+    //        }
+    //
+    //        return Dictionary(grouping: filtered, by: { $0.category }).map { $0.key }
+    //    }
 }
 
 struct MonthlyItem {
@@ -493,91 +314,6 @@ struct ChangingGraph: View {
     }
 }
 
-struct CategoryPieChart: View {
-    fileprivate var items: [ChartData]
-
-    var body: some View {
-        Chart {
-            ForEach(items) { item in
-                SectorMark(
-                    angle: .value("category", item.value),
-                    innerRadius: .ratio(0.6),
-                    outerRadius: .ratio(1.0),
-                    angularInset: 10
-                )
-                .foregroundStyle(by: .value("category", item.category))
-            }
-        }
-        .chartLegend(.hidden)
-    }
-}
-
-private let percentageFomatter: NumberFormatter = {
-    let fomatter = NumberFormatter()
-    fomatter.numberStyle = .percent
-    return fomatter
-}()
-struct StatisticsMonthlyChart: View {
-    fileprivate var items: [ChartData]
-    private var sum: Double { self.items.reduce(0, { $0 + $1.value }) }
-
-    var body: some View {
-        Chart {
-            ForEach(self.items) { item in
-                BarMark(
-                    x: .value("amount", item.value),
-                    y: .value("category", item.category),
-                    width: .fixed(20)
-                )
-                .foregroundStyle(by: .value("category", item.category))
-                .annotation(position: .trailing) {
-                    let percentage = percentageFomatter.string(from: NSNumber(value: item.value / self.sum))
-                    Text(percentage ?? "?")
-                }
-            }
-        }
-        .frame(height: 60 * CGFloat(self.items.count))
-    }
-}
-
-struct StatisticsChart<Destination>: View where Destination: View {
-    var title: String
-    var selectedCategories: [String]
-    fileprivate var items: [ChartData]
-
-    @ViewBuilder var destination: () -> Destination
-
-    var body: some View {
-        HStack {
-            Text(self.title)
-                .font(.title)
-            Spacer()
-            NavigationLink {
-                self.destination()
-            } label: {
-                Image(systemName: "checkmark.square")
-            }
-        }
-
-        if items.isEmpty {
-            Text("Empty Data")
-                .font(.title)
-                .frame(height: 200)
-        } else {
-            Chart {
-                ForEach(items) { item in
-                    BarMark(
-                        x: .value("month", item.yearMonth),
-                        y: .value("amount", item.value)
-                    )
-                    .foregroundStyle(by: .value("category", item.category))
-                }
-            }
-            .frame(height: 200)
-        }
-    }
-}
-
 extension Date {
     fileprivate func getYear() -> Int {
         return Calendar.current.component(.year, from: self)
@@ -593,13 +329,6 @@ private let formatter: NumberFormatter = {
     formatter.numberStyle = .decimal
     return formatter
 }()
-
-struct ChartView_Previews: PreviewProvider {
-    static var previews: some View {
-        ChartView()
-            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
 
 struct EqualSizeHStack: Layout {
     func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
@@ -620,4 +349,9 @@ struct EqualSizeHStack: Layout {
         }
     }
 
+}
+
+#Preview {
+    ChartView()
+        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
