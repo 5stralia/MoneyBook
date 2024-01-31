a34d33b8cb6a88b7358513a544d8fe17af27f801
Hoju Choi <5stralia@gmail.com>
2024-02-01 00:00:40 +0900

    feat: 금액 실시간 포맷팅

--- MoneyBook/AppendingItem/AppendingItemView.swift
+++ MoneyBook/AppendingItem/AppendingItemView.swift
@@ -44,24 +44,204 @@ struct AppendingItemTypeView: View {
 struct AppendingItemNumberInputView: View {
     let image: Image
     let title: String
-
-    @Binding var value: Double?
+    @StateObject var doubleStore: NumberStore<Double, FloatingPointFormatStyle<Double>>
 
     var body: some View {
         HStack(spacing: 0) {
             image
                 .frame(width: 28)
                 .padding(.all, 8)
-            TextField("Amount", value: $value, format: .number)
+            TextField("Amount", text: $doubleStore.text)
+                .formatAndValidate(doubleStore) { $0 > 0 }
                 .keyboardType(.decimalPad)
-                .onReceive(Just(value)) { newValue in
-                    guard let newValue, newValue < 0 else { return }
-                    self.value = -newValue
-                }
             Spacer()
         }
         .background()
         .cornerRadius(8)
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func formatAndValidate<T: Numeric, F: ParseableFormatStyle>(
+        _ numberStore: NumberStore<T, F>, errorCondition: @escaping (T) -> Bool
+    ) -> some View {
+        onChange(of: numberStore.text) { text in
+            numberStore.error = false
+            if let value = numberStore.getValue(), !errorCondition(value) {
+                numberStore.error = false
+            } else if text.isEmpty || text == numberStore.minusCharacter {
+                numberStore.error = false
+            } else {
+                numberStore.error = true
+            }
+        }
+        //        .foregroundColor(numberStore.error ? .red : .primary)
+        .disableAutocorrection(true)
+        .autocapitalization(.none)
+        .onSubmit {
+            if numberStore.text.count > 1 && numberStore.text.suffix(1) == numberStore.decimalSeparator {
+                numberStore.text.removeLast()
+            }
+        }
+    }
+}
+
+class NumberStore<T: Numeric, F: ParseableFormatStyle>: ObservableObject
+where F.FormatOutput == String, F.FormatInput == T {
+    @Published var text: String
+    let type: ValidationType
+    let maxLength: Int
+    let allowNegative: Bool
+    private var backupText: String
+    @Published var error: Bool = false
+    private let locale: Locale
+    let formatter: F
+
+    init(
+        text: String = "",
+        type: ValidationType,
+        maxLength: Int = 18,
+        allowNegative: Bool = false,
+        formatter: F,
+        locale: Locale = .current
+    ) {
+        self.text = text
+        self.type = type
+        self.allowNegative = allowNegative
+        self.formatter = formatter
+        self.locale = locale
+        backupText = text
+        self.maxLength = maxLength == .max ? .max - 1 : maxLength
+    }
+
+    var result: T? {
+        try? formatter.parseStrategy.parse(text)
+    }
+
+    func restore() {
+        text = backupText
+    }
+
+    func backup() {
+        backupText = text
+    }
+
+    lazy var decimalSeparator: String = {
+        locale.decimalSeparator ?? "."
+    }()
+
+    private lazy var groupingSeparator: String = {
+        locale.groupingSeparator ?? ""
+    }()
+
+    let minusCharacter = "-"
+
+    private lazy var characters: String = {
+        let number = "0123456789"
+        switch type {
+        case .int:
+            return number + (allowNegative ? minusCharacter : "")
+        case .double:
+            return number + (allowNegative ? minusCharacter : "") + decimalSeparator
+        }
+    }()
+
+    var minusCount: Int {
+        text.components(separatedBy: minusCharacter).count - 1
+    }
+
+    // 检查是否为有效输入字符
+    func characterValidator() -> Bool {
+        text.allSatisfy { characters.contains($0) }
+    }
+
+    // 返回验证后的数字
+    func getValue() -> T? {
+        // 特殊处理（无内容、只有负号、浮点数首字母为小数点）
+        if text.isEmpty || text == minusCharacter || (type == .double && text == decimalSeparator) {
+            backup()
+            return nil
+        }
+
+        // 用去除组分隔符后的字符串判断字符是否有效
+        let pureText = text.replacingOccurrences(of: groupingSeparator, with: "")
+        guard pureText.allSatisfy({ characters.contains($0) }) else {
+            restore()
+            return nil
+        }
+
+        // 处理多个小数点情况
+        if type == .double {
+            if text.components(separatedBy: decimalSeparator).count > 2 {
+                restore()
+                return nil
+            }
+        }
+
+        // 多个负号情况
+        if minusCount > 1 {
+            restore()
+            return nil
+        }
+
+        // 负号必须为首字母
+        if minusCount == 1, !text.hasPrefix("-") {
+            restore()
+            return nil
+        }
+
+        // 判断长度
+        guard text.count < maxLength + minusCount else {
+            restore()
+            return nil
+        }
+
+        // 将文字转换成数字，然后在转换为文字（保证文字格式正确）
+        if let value = try? formatter.parseStrategy.parse(text) {
+            let hasDecimalCharacter = text.contains(decimalSeparator)
+            let zeroCount = text.trailingZerosCountAfterDecimal()
+            text = formatter.format(value)
+            // 保护最后的小数点（不特别处理的话，转换回来的文字可能不包含小数点）
+            if hasDecimalCharacter, !text.contains(decimalSeparator) {
+                text.append(decimalSeparator)
+                text += (0..<zeroCount).map { _ in "0" }.joined(separator: "")
+            }
+            backup()
+            return value
+        } else {
+            restore()
+            return nil
+        }
+    }
+
+    enum ValidationType {
+        case int
+        case double
+    }
+}
+
+extension String {
+    func trailingZerosCountAfterDecimal() -> Int {
+        var count = 0
+        var foundDecimal = false
+
+        for char in self.reversed() {
+            if char == "." {
+                foundDecimal = true
+                break
+            }
+        }
+
+        for char in self.reversed() {
+            if foundDecimal && char == "0" {
+                count += 1
+            } else if char != "0" {
+                break
+            }
+        }
+
+        return count
     }
 }
 
@@ -167,6 +347,7 @@ struct AppendingItemView: View {
 
     @State private var title: String
     @State private var amount: Double?
+    @StateObject var doubleStore: NumberStore<Double, FloatingPointFormatStyle<Double>>
     @State private var date: Date
     @State private var isPaid: Bool
     @State private var selection: String
@@ -179,12 +360,34 @@ struct AppendingItemView: View {
         if let item {
             self._title = State(initialValue: item.title)
             self._amount = State(initialValue: abs(item.amount))
+            self._doubleStore = StateObject(
+                wrappedValue: NumberStore(
+                    text: abs(item.amount).formatted(),
+                    type: .double,
+                    maxLength: 12,
+                    allowNegative: false,
+                    formatter: FloatingPointFormatStyle<Double>()
+                        .precision(.fractionLength(0...2))
+                        .rounded(rule: .towardZero)
+                )
+            )
             self._date = State(initialValue: item.timestamp)
             self._isPaid = State(initialValue: item.amount < 0)
             self._selection = State(initialValue: item.category)
         } else {
             self._title = State(initialValue: "")
             self._amount = State(initialValue: nil)
+            self._doubleStore = StateObject(
+                wrappedValue: NumberStore(
+                    text: "",
+                    type: .double,
+                    maxLength: 12,
+                    allowNegative: false,
+                    formatter: FloatingPointFormatStyle<Double>()
+                        .precision(.fractionLength(0...2))
+                        .rounded(rule: .towardZero)
+                )
+            )
             self._date = State(initialValue: Date())
             self._isPaid = State(initialValue: true)
             self._selection = State(initialValue: "기타")
@@ -203,7 +406,7 @@ struct AppendingItemView: View {
                 AppendingItemTextInputView(
                     image: Image(systemName: "t.square"), title: "Title", text: $title)
                 AppendingItemNumberInputView(
-                    image: Image(systemName: "dollarsign"), title: "Amount", value: $amount)
+                    image: Image(systemName: "dollarsign"), title: "Amount", doubleStore: self.doubleStore)
                 AppendingItemDateInputView(date: $date)
                 AppendingItemCategoryInputView(selection: $selection)
                     .background(.background)
