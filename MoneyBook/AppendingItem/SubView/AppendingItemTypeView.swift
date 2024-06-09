fdb45596afb5bea9d8bcffe234d2a212813ca9a3
Hoju Choi <5stralia@gmail.com>
2024-06-09 15:36:35 +0900

    fix: 지출/소득 toggle 애니메이션

--- MoneyBook/AppendingItem/SubView/AppendingItemTypeView.swift
+++ MoneyBook/AppendingItem/SubView/AppendingItemTypeView.swift
@@ -7,48 +7,43 @@
 
 import SwiftUI
 
-struct AppendingItemTypeView: View {
-    @Binding var isExpense: Bool
-    let didChangeType: (_ newValue: Bool) -> Void
-
-    var body: some View {
+struct AppendingItemTypeToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
         Capsule()
-            .fill(isExpense ? Color.customOrange1.opacity(0.3) : Color.customIndigo1.opacity(0.3))
-            .overlay {
-                HStack(spacing: 0) {
-                    Capsule()
-                        .fill(isExpense ? Color.customOrange1 : .clear)
-                        .overlay {
-                            Button {
-                                withAnimation {
-                                    self.isExpense = true
-                                }
-                                didChangeType(true)
-                            } label: {
-                                Text("Expense")
-                                    .font(.Pretendard(size: 15, weight: .bold))
-                                    .foregroundColor(.white)
-                            }
-                        }
-                    Capsule()
-                        .fill(isExpense ? .clear : Color.customIndigo1)
-                        .overlay {
-                            Button {
-                                withAnimation {
-                                    self.isExpense = false
-                                }
-                                didChangeType(false)
-                            } label: {
-                                Text("Income")
-                                    .font(.Pretendard(size: 15, weight: .bold))
-                                    .foregroundColor(.white)
-                            }
-                        }
+            .fill(configuration.isOn ? Color.customOrange1.opacity(0.3) : Color.customIndigo1.opacity(0.3))
+            .frame(width: 118, height: 32)
+            .overlay(alignment: configuration.isOn ? .leading : .trailing) {
+                Capsule()
+                    .fill(configuration.isOn ? Color.customOrange1 : Color.customIndigo1)
+                    .frame(width: 118 / 2, height: 30)
+                    .overlay {
+                        Text(configuration.isOn ? "Expense" : "Income")
+                            .font(.Pretendard(size: 15, weight: .bold))
+                            .foregroundColor(.white)
+                    }
+            }
+            .onTapGesture {
+                withAnimation {
+                    configuration.isOn.toggle()
                 }
             }
     }
 }
 
+private struct Test: View {
+    @State var isOn: Bool = false
+
+    var body: some View {
+        Toggle(
+            isOn: $isOn,
+            label: {
+
+            }
+        )
+        .toggleStyle(AppendingItemTypeToggleStyle())
+    }
+}
+
 #Preview {
-    AppendingItemTypeView(isExpense: .constant(true), didChangeType: { _ in })
+    Test()
 }
